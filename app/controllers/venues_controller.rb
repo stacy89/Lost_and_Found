4d5f3b7566d0ea616bcb807373a49da39523a227
Stacy Areas <stacyareas@gmail.com>
2017-04-21 23:06:15 -0700

    add venue routes that allows a buisness owner to login and got to profile page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,14 +1,20 @@
-get '/venues/new' do 
-	erb :'venues/new'
-end 
+get '/venues/_new' do 
+	erb :'venues/_new'
+end # Not sure if this should point to the user login of the partial
 
-post 'venues' do 
+post '/venues' do 
 	@venue = Venue.new(params[:venue])
 	if @venue.save
 		redirect "/venues/#{@venue.id}"
 	else
 		@errors = @venue.errors.full_messages
-		erb :'venues/new'
+		erb :'users/new'
 	end
+end
+
+get '/venues' do 
+	@venue = Venue.find(params[:id])
+	redirect '/sessions/new' unless logged_in?
+	erb :'venues/show'
 end
 
